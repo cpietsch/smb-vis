cc6e91152f0df8c70ca7a3d8fd28cb8322a39137
Christopher Pietsch <cpietsch@gmail.com>
2020-09-30 09:16:36 +0200

    decoupled annotation sizes from screen with

--- src/Annotations.svelte
+++ src/Annotations.svelte
@@ -2,7 +2,7 @@
   import { onMount, getContext, onDestroy, tick } from "svelte";
   import { scaleLinear, scaleLog } from "d3-scale";
   import { quadtree as d3quadtree } from "d3-quadtree";
-  import { scales, lastTransformed } from "./stores.js";
+  import { scales, lastTransformed, dimensions } from "./stores.js";
   import { select, pointer } from "d3-selection";
   import { interpolate as d3interpolate } from "d3-interpolate";
   import { get } from "svelte/store";
@@ -16,8 +16,21 @@
 
   const baseUrl = "annotations/";
   const sprites = [];
+  const imageWidth = 1300;
   let subsription;
-  const size = scaleLog().range([0.12, 0.02]).domain([1, 20]).clamp(true);
+  let factor = 0.1;
+  const sizeTable = { 1: 1.7, 2: 1.3, 3: 1 };
+  const size = scaleLog()
+    .range([factor, factor / 5])
+    .domain([1, 20])
+    .clamp(true);
+
+  $: {
+    factor =
+      (Math.log($dimensions.width * $dimensions.height) / imageWidth) * 10;
+    size.range([factor, factor / 5]);
+    console.log(factor);
+  }
 
   onMount(async () => {
     const { annotations } = await json(baseUrl + "annotations.json");
@@ -25,8 +38,11 @@
       //   console.log(annotation);
 
       const sprite = Sprite.from(baseUrl + annotation.name + ".png");
-      sprite.scale.x = sprite.scale.y =
-        ((3 - annotation.size) / 2) * 0.04 + size(1);
+      // sprite.on("added", (s) => {
+      //   console.log(s.width);
+      // });
+      // sprite.scale.x = sprite.scale.y =
+      //   ((3 - annotation.size) / 2) * 0.04 + size(1);
       sprite.anchor.set(0.5);
       sprite.x = $scales.x(annotation.x);
       sprite.y = $scales.y(annotation.y);
@@ -38,8 +54,9 @@
     subsription = lastTransformed.subscribe((t) => {
       //   console.log(t.k, size(t.k));
       for (const sprite of sprites) {
-        sprite.scale.x = sprite.scale.y =
-          ((3 - sprite.__asize) / 2) * 0.04 + size(t.k);
+        sprite.scale.x = sprite.scale.y = sizeTable[sprite.__asize] * size(t.k);
+        // sprite.scale.x = sprite.scale.y =
+        //   ((3 - sprite.__asize) / 2) * 0.04 + size(t.k);
       }
     });
     // console.log(annotations);
