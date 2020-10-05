e197200ac8c36e1e044de9d447aec626d8b0c0b0
Chrristopher Pietsch <cpietsch@gmail.com>
2020-10-05 20:46:56 +0200

    svite-plugin instead of svite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
-// module.exports = {
-//     optimizeDeps: {
-//         allowNodeBuiltinss: ["seedrandom"],
-//          exclude: ['seedrandom'],
-//     }
-// }
+import svelte from 'vite-plugin-svelte';
 
-// const svite = require('svite');
-// const sviteConfig = {
-//     hot: true, // boolean or options object for svelte-hmr
-//     useTransformCache: false, // boolean
-// };
-// module.exports = {
-//     optimizeDeps: {
-//         allowNodeBuiltins: ["seedrandom"]
-//     },
-//     // if you provite a svite plugin here, svite cli will use it instead of initializing one for you
-//     plugins: [svite(sviteConfig)],
-//     // if you don't use svite cli, you should provide rollupDedupe option, otherwise you risk duplicate svelte instances and errors
-//     rollupDedupe: ['svelte/animate', 'svelte/easing', 'svelte/internal', 'svelte/motion', 'svelte/store', 'svelte/transition', 'svelte'],
-// };
+export default {
+  plugins: [svelte()],
+  rollupDedupe: ['svelte']
+};
