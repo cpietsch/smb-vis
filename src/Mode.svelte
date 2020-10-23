1b6be307d36bd278455bdf765e4c2f934617f61d
Christopher Pietsch <cpietsch@gmail.com>
2020-10-23 20:54:47 +0200

    mode

---
+++ src/Mode.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { darkmode } from "./stores.js";
+
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    darkmode.set(true)
+  }
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+      const newColorScheme = e.matches ? "dark" : "light";
+      darkmode.set(newColorScheme)
+  });
+</script>
+
+<style>
+  .mode {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    z-index: 300;
+    cursor: pointer;
+    padding: 5px
+  }
+  .icon {
+    width: 18px;
+    height: 18px;
+    background: url("moon.svg")
+  }
+  .dark {
+    background: url("sun.svg")
+  }
+</style>
+
+<div class="mode">
+  <div class="icon" class:dark={$darkmode} on:click={ () => darkmode.update(d => !d) } />
+</div>
